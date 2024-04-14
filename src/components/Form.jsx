b98a39e427ee8935b4151b42393f9856c0510268
Illia Movchko <illia.movchko.concept@gmail.com>
2024-04-14 08:44:44 +0000

    Update Form styles

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -17,13 +17,14 @@ export default function Form({ addToDo }) {
 
   return (
     <form
-      className="w-full flex justify-between items-center border rounded overflow-hidden"
+      className="w-full p-px flex justify-center items-center rounded bg-gradient-to-r from-primary to-secondary"
       onSubmit={handleSubmit}
     >
       <input
-        className="w-full p-4 flex justify-between items-center"
+        className="w-full p-4 flex justify-center items-center rounded outline-none text-text text-opacity-90 bg-bg bg-opacity-90 placeholder:text-text placeholder:text-opacity-50"
         name="input"
         type="text"
+        placeholder="To-Do"
         value={toDoText}
         onChange={handleChange}
       ></input>
--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,30 +1,42 @@
 export default function ToDoItem({ item, checkToDo, deleteToDo }) {
   return (
-    <li className="w-full p-px rounded bg-gradient-to-r from-primary to-secondary">
+    <li
+      className={`w-full p-px rounded bg-gradient-to-r from-primary to-secondary ${
+        item.completed ? "opacity-50" : ""
+      }`}
+    >
       <div className="w-full p-4 flex justify-between items-center rounded bg-bg bg-opacity-90">
         <div className="flex justify-between items-center">
           <button
-            className="w-8 h-8 border rounded bg-primary"
+            className="w-8 h-8 border border-text border-opacity-50 rounded bg-primary bg-opacity-50"
             aria-label="check"
             onClick={() => checkToDo(item.id)}
           >
-            {item.completed ? <i className="fa-solid fa-check"></i> : null}
+            {item.completed ? (
+              <i className="fa-solid fa-check opacity-90"></i>
+            ) : null}
           </button>
         </div>
-        <span className="">{item.text}</span>
+        <span
+          className={`text-text text-opacity-90 ${
+            item.completed ? "line-through" : ""
+          }`}
+        >
+          {item.text}
+        </span>
         <div className="flex justify-center items-center gap-4">
           <button
-            className="w-8 h-8 border rounded bg-secondary"
+            className="w-8 h-8 border border-text border-opacity-50 rounded bg-secondary bg-opacity-50"
             aria-label="edit"
           >
-            <i class="fa-solid fa-pen"></i>
+            <i class="fa-solid fa-pen opacity-90"></i>
           </button>
           <button
-            className="w-8 h-8 border rounded bg-delete"
+            className="w-8 h-8 border border-text border-opacity-50 rounded bg-delete bg-opacity-50"
             aria-label="delete"
             onClick={() => deleteToDo(item.id)}
           >
-            <i class="fa-solid fa-trash"></i>
+            <i class="fa-solid fa-trash opacity-90"></i>
           </button>
         </div>
       </div>
